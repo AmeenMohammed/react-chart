35f2c5bd12f5327990771ee1e590490933d1490e
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2021-08-21 14:56:40 +0200

    Get quetions and answers

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,5 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
     'no-console': 'off',
-
   },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import DatePickerComponent from '../components/DatePicker';
+import { formatDate } from '../utils/functions';
 
 const Home = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
 
+  function getAnswers(dateFrom, dateTo) {
+    axios.get(`${process.env.REACT_APP_ANSWERS_URL}?date_from=${formatDate(dateFrom)}&date_to=${formatDate(dateTo)}`, {
+      headers: {
+        authorization: 'Bearer SLSmxK17vjRInEWIiFQjwE1QIDfeSM',
+      },
+    })
+      .then((res) => {
+        setAnswers(res.data);
+      }).catch((err) => {
+        console.error(`Something went wrong !${err}`);
+      });
+  }
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_QUESTIONS_URL}`)
+      .then((res) => {
+        setQuestions(res.data);
+      }).catch((err) => {
+        console.error(`Something went wrong !${err}`);
+      });
+    getAnswers(startDate, endDate);
+  }, [startDate, endDate]);
+
+  const handleDateChange = (date, type) => {
+    if (type === 'from') {
+      setStartDate(date);
+      getAnswers(date, endDate);
+    } else {
+      setEndDate(date);
+      getAnswers(startDate, date);
+    }
+  };
+
+  console.log(questions);
+  console.log(answers);
   return (
     <>
-      <DatePickerComponent datevalue={startDate} changeDate={setStartDate} dateLabel="Date from: " />
-      <DatePickerComponent datevalue={endDate} changeDate={setEndDate} dateLabel="Date to: " />
+      <DatePickerComponent datevalue={startDate} changeDate={(date) => handleDateChange(date, 'from')} dateLabel="Date from: " />
+      <DatePickerComponent datevalue={endDate} changeDate={(date) => handleDateChange(date, 'to')} dateLabel="Date to: " />
     </>
   );
 };
